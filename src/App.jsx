ac69faac5202a4406d5c20ef4d01d94fad2d9b84
hevardhan <paatukkorupulavan@gmail.com>
2024-08-30 10:40:45 +0530

    Bitch, U have to add the component in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import AboutUs from './components/AboutUs'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
       <Navbar />
+      <AboutUs/> 
     </>
   )
 }
