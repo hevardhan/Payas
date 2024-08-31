c1aa436faa6b033b18f0faf36be2f04a8f61901e
Rohan <rohaningle911@gmail.com>
2024-08-31 11:17:41 +0530

    Change the line stuff and add virtual padding ™️

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import './App.css'
-import AboutUs from './components/AboutUs'
-import Home from './components/Home'
-import Navbar from './components/Navbar'
+import './App.css';
+import AboutUs from './components/AboutUs';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 import React, { useEffect } from "react";
 
 function App() {
   useEffect(() => {
     window.location.hash = '#home';
   }, []);
+  
   return (
     <>
       <Navbar />
-    <section id="home">
-      <Home />
-    </section>
-    <section id="about">
-      <AboutUs/> 
-    </section>
+      <section id="home" className="section-padding">
+        <Home />
+      </section>
+      <section id="about" className="section-padding">
+        <AboutUs /> 
+      </section>
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import './css/Home.css';
 const Home = () => {
   return (
     <div className='container main pt-2 align-items-center justify-content-center d-flex flex-column'>
-      <div className="by-line text-center col-7">
+      <div className="by-line text-center col-7" style={{marginBottom: '2vh'}}>
         <p className='display-5'>Your Gateway to IT Excellence and Career Growth</p>
       </div>
       <div className="home-buttons d-flex my-3 gap-5">
@@ -15,12 +15,12 @@ const Home = () => {
             Contact Us
           </button>
       </div>
-      <div className="home-img text-center col-lg-4 mt-4 col-sm-1 px-sm-2">
-        <img src="/home-img.png" alt=""/>
+      <div className="home-img text-center col-lg-4 mt-2 col-sm-1 px-sm-2" style={{height: '50vh'}}>
+        <img src="/home-img.png" alt="" style={{maxHeight: '100%', maxWidth: '100%', objectFit: 'contain'}}/>
       </div>
-      <div className="home-footer d-flex align-items-center justify-content-center text-center mt-5 mx-5">
+      <div className="home-footer d-flex align-items-center justify-content-center text-center mt-4 mx-5" style={{height: '20vh'}}>
         <div className="foot-left col-lg-4 col-sm-12">
-          <p>Empowering IT professionals with top-tier training,ensuring clarity and expertise in every career step</p>
+          <p>Empowering IT professionals with top-tier training, ensuring clarity and expertise in every career step</p>
         </div>
         <div className="foot-center col-lg-4 col-sm-12">
           <p>Shaping the next generation of IT leaders with cutting-edge training and strategic placements for impactful careers</p>
@@ -32,5 +32,6 @@ const Home = () => {
     </div>
   )
 }
+
 
 export default Home
